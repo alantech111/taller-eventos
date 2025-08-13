fc97faf325af1dfa85c06332196097963f3aba97
alantech111 <90072756+alantech111@users.noreply.github.com>
2025-08-12 21:27:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById('contenedor').addEventListener('click', function() {
+document.getElementById('contenedor').addEventListener('click', function(e) {
+  if (e.target === this) {  // Solo se ejecuta si el clic fue en el div (no en sus hijos)
     alert('Hola! Soy el div');
+  }
+});
+
+document.querySelector('button').addEventListener('click', function(e) {
+  e.stopPropagation();  // Evita que el evento llegue al div
+  alert('Hola!');
 });
